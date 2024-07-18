43086bd57f4c4cd55156c2699b1844d2b874f4cd
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2024-07-18 11:14:36 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  int* arr = new int[1001];
+  
+  for( int i =0 ; i <= n; i++){
+    if( i == 1 || i == 0){
+      arr[i] = 1;
+    }else{
+      arr[i] = (arr[i-1] + (arr[i-2]*2))% 10007;
+    }
+  }
+  cout << arr[n];
+}
