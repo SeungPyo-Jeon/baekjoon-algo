7b3754f27ef8a223f0a1547a88a21ca7aae40f83
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2024-07-18 11:15:27 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for( int i = 1; i<= n ; i++){
+        for( int j = 1; j <=n ; j++){
+          if( n-i >= j ) cout <<" ";
+          else cout << "*";
+        }
+       cout << endl;
+    }
+    for( int i = n-1; i > 0 ; i--){
+        for( int j = 1; j <=n ; j++){
+          if( j > n-i) cout << "*";
+          else cout << " ";
+        }
+       cout << endl;
+    }
+}
