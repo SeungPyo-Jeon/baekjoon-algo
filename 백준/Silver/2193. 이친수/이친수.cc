0875b81b6a442134a7c7c9f12ca0e39b5321a5df
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2024-07-18 11:13:58 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  long arr[91] = {0};
+  int N;
+  cin >> N;
+  arr[1]=1;
+  arr[2]=1;
+  for( int i =3; i <= N; i++){
+    arr[i] = arr[i-1] + arr[i-2];
+  }
+  cout << arr[N];
+}
