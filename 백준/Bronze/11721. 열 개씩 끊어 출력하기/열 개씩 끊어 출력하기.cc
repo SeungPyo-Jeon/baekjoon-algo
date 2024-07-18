98c195d5d87ba6b6dedbf9ffc106fb019fc275f7
SeungPyo Jeon <54004940+SeungPyo-Jeon@users.noreply.github.com>
2024-07-18 11:17:01 +0900

    [Bronze III] Title: 열 개씩 끊어 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    getline(cin , str);
+    while( str.length()/10 ){
+       cout << str.substr(0,10) << endl;
+       str = str.substr( 10);
+    }
+    cout << str;
+    return 0;
+}
